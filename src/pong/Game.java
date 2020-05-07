d8813d40b0e8cb2cbbf23dcdd0534e4eb46efe81
kirakun123 <igorjuliosilva@gmail.com>
2020-05-07 10:28:46 +0100

    Initial commit

---
+++ src/pong/Game.java
@@ -0,0 +1,48 @@
+package pong;
+
+import java.awt.Canvas;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Game extends Canvas implements Runnable, KeyListener{
+	
+	/**
+	 * @param WIDTH
+	 * 
+	 * Esta constante foi criada para deixar uma dimensão pré-estabelecida da janela do jogo.
+	 * 
+	 * @param HEIGHT
+	 * 
+	 * Esta constante foi criada para deixar uma dimensão pré-estabelecida da janela do jogo.
+	 * 
+	 * @param SCALE
+	 * 
+	 * Uma constante para aumentar a escala da resolução e deixar a aplicação mais leve.
+	 */
+	private static final long serialVersionUID = 1L;
+	public static int WIDTH = 160;
+	public static int HEIGHT = 120;
+	public static int SCALE = 3;
+
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+}
